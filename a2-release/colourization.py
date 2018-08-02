1698f4c25676c9075ab4d72ec5bdbf0904a24ab0
Yuchen Pei <me@ypei.me>
2018-08-02 16:52:32 +0200

    finished unet

--- a2-release/colourization.py
+++ a2-release/colourization.py
@@ -239,20 +239,47 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
 
         ############### YOUR CODE GOES HERE ############### 
+        self.downconv1 = nn.Sequential(
+            MyConv2d(1, num_filters, kernel_size=kernel),
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(num_filters),
+            nn.ReLU())
+        self.downconv2 = nn.Sequential(
+            MyConv2d(num_filters, num_filters*2, kernel_size=kernel),
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(num_filters*2),
+            nn.ReLU())
 
+        self.rfconv = nn.Sequential(
+            MyConv2d(num_filters*2, num_filters*2, kernel_size=kernel),
+            nn.BatchNorm2d(num_filters*2),
+            nn.ReLU())
+
+        self.upconv1 = nn.Sequential(
+            MyConv2d(num_filters*4, num_filters, kernel_size=kernel),
+            nn.Upsample(scale_factor=2),
+            nn.BatchNorm2d(num_filters),
+            nn.ReLU())
+        self.upconv2 = nn.Sequential(
+            MyConv2d(num_filters*2, 24, kernel_size=kernel),
+            nn.Upsample(scale_factor=2),
+            nn.BatchNorm2d(24),
+            nn.ReLU())
+
+        self.finalconv = MyConv2d(25, 24, kernel_size=kernel)
         ###################################################
 
     def forward(self, x):
         ############### YOUR CODE GOES HERE ############### 
-        #self.out1 = ...
-        #self.out2 = ...
-        #self.out3 = ...
-        #self.out4 = ...
-        #self.out5 = ...
-        #self.out_final = ...
-        #return self.out_final
+        self.out1 = self.downconv1(x)
+        self.out2 = self.downconv2(self.out1)
+        self.out3 = self.rfconv(self.out2)
+        self.out4 = self.upconv1(torch.cat((self.out3, self.out2), 1))
+        self.out5 = self.upconv2(torch.cat((self.out4, self.out1), 1))
+        self.out_final = self.finalconv(torch.cat((self.out5, x), 1))
+        return self.out_final
         ###################################################
-        pass
+        #pass
 
 class DilatedUNet(UNet):
     def __init__(self, kernel, num_filters, num_colours):
